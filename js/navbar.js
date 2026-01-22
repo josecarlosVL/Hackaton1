79f7ca7f3436d769237db4f4aa5f53a8635938e0
Jose Carlos Velasco <jcvl0611@gmail.com>
2026-01-22 15:35:44 -0600

    feat: navbar como archivo js implementado

---
+++ js/navbar.js
@@ -0,0 +1,80 @@
+const contenedor = document.getElementById("contenedor-navbar");
+
+const navbarHTML = `
+<style>
+.navbar{
+    font-family: 'Oswald',sans-serif;
+    font-size: x-large;
+}
+.nav-item > a > div{
+    font-size: large;
+}
+#input-busqueda
+{
+    padding: 0px;
+}
+
+#boton-busqueda{
+    height:40px;
+    margin: 0px;
+    border: 0px;
+}
+.nav-link:hover{
+    color: var(--color3-vpasto) !important;
+}
+.btn:hover{
+    background-color: var(--color3-vpasto) !important;
+}
+</style>
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/"><img src="/images/zunzo.png"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 align-items-center">
+        <li class="nav-item  d-lg-none">
+          <a class="nav-link" href="/">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about.html">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Calzado</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ropa</a>
+        </li>
+        <li class="nav-item  d-lg-none">
+          <a class="nav-link" href="#">Login/Register</a>
+        </li>
+        <li class="nav-item  d-lg-none">
+          <a class="nav-link" href="#">Carrito</a>
+        </li>
+        <li class="nav-item d-none d-lg-flex align-items-center mx-3">
+            <div class="input-group">
+                <input type="text" id="input-busqueda" class="form-control" placeholder="Buscar producto..." aria-label="Buscar">
+                <button id="boton-busqueda" class="btn btn-dark fw-bold px-4" type="button">
+                    BUSCAR
+                </button>
+            </div>
+        </li>
+        <li class="nav-item d-none d-lg-block"></li>
+        <li class="nav-item d-none d-lg-flex flex-column align-items-center mx-3">
+            <a href="#" class="nav-link p-0 text-center text-dark">
+                <img src="/images/user.png" width="36px" height="36px" class="mb-1" alt="User">
+                <div>Login/Register</div>
+            </a>
+        </li>
+        <li class="nav-item d-none d-lg-flex flex-column align-items-center mx-3">
+            <a href="#" class="nav-link p-0 text-center text-dark">
+                <img src="/images/carrito.png" width="36px" height="36px" class="mb-1" alt="Cart">
+                <div>Carrito</div>
+            </a>
+        </li>
+    </div>
+  </div>
+</nav>
+`
+contenedor.innerHTML = navbarHTML;
--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // js/script.js
 document.addEventListener("DOMContentLoaded", () => {
+  //Cosas para el dropdown
+          function toggleDropdown() {
+            const dropdown = document.getElementById('dropdownMenu');
+            dropdown.classList.toggle('show');
+        }
   const heroCarouselEl = document.querySelector('#heroCarousel');
   if (!heroCarouselEl) return;
+          window.onclick = function(event) {
+            if (!event.target.matches('.category-dropdown') && !event.target.matches('.category-dropdown *')) {
+                const dropdown = document.getElementById('dropdownMenu');
+                if (dropdown.classList.contains('show')) {
+                    dropdown.classList.remove('show');
+                }
+            }
+        }
 
   // Inicializar el carrusel
   const carousel = new bootstrap.Carousel(heroCarouselEl, {
@@ -20,41 +33,40 @@ document.querySelectorAll('.product-btn').forEach(btn => {
 
 
 
-
-
-
-
-//testimonios
-var multipleCardCarousel = document.querySelector("#carouselExampleControls");
+// testimonios
+var multipleCardCarousel = document.querySelector(
+  "#carouselExampleControls.testimonial-carousel"
+);
 
 if (window.matchMedia("(min-width: 576px)").matches) {
   var carousel = new bootstrap.Carousel(multipleCardCarousel, {
     interval: false
   });
-  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-  var cardWidth = $(".carousel-item").width();
+
+  var $inner = $("#carouselExampleControls.testimonial-carousel .carousel-inner");
+  var carouselWidth = $inner[0].scrollWidth;
+  var cardWidth = $("#carouselExampleControls.testimonial-carousel .carousel-item").width();
   var scrollPosition = 0;
-  $("#carouselExampleControls .carousel-control-next").on("click", function () {
-    if (scrollPosition < carouselWidth - cardWidth * 1) {
-      scrollPosition += cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        0
-      );
-    }
-  });
-  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-    if (scrollPosition > 0) {
-      scrollPosition -= cardWidth;
-      $("#carouselExampleControls .carousel-inner").animate(
-        { scrollLeft: scrollPosition },
-        0
-      );
-    }
-  });
+
+  $("#carouselExampleControls.testimonial-carousel .carousel-control-next")
+    .on("click", function () {
+      if (scrollPosition < carouselWidth - cardWidth) {
+        scrollPosition += cardWidth;
+        $inner.animate({ scrollLeft: scrollPosition }, 0);
+      }
+    });
+
+  $("#carouselExampleControls.testimonial-carousel .carousel-control-prev")
+    .on("click", function () {
+      if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        $inner.animate({ scrollLeft: scrollPosition }, 0);
+      }
+    });
 } else {
   $(multipleCardCarousel).addClass("slide");
 }
+
 
 
 
