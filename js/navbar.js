0e86f520d8174efea6b715eee0bd8a89b11d0d89
HectorMagdaleno <estoreals@gmail.com>
2026-01-22 23:31:16 -0600

    Fix: Cambios navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,13 @@
 const contenedor = document.getElementById("contenedor-navbar");
 
+
 const navbarHTML = `
 <style>
 .navbar{
     font-family: 'Oswald',sans-serif;
     font-size: x-large;
 }
-.nav-item > a > div{x
+.nav-item > a > div{
     font-size: large;
 }
 #input-busqueda
@@ -28,7 +29,7 @@ const navbarHTML = `
 </style>
 <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/"><img src="../images/zunzo.png"></a>
+    <a class="navbar-brand" href="/"><img src="./images/zunzo.png"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
